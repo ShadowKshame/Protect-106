f451e2e18ea30cdeaeb16f2a72df0f7ce1efff0e
ShadowKshame <104213448+ShadowKshame@users.noreply.github.com>
2022-11-29 20:35:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //https://teachablemachine.withgoogle.com/models/uzveQOiud/
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/uzveQOiud/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
